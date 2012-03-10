72baff86831bfd6eff6f0ac70b2482b195a2506a
Jeff Jones <jjones027@yahoo.com>
2012-03-10 16:33:08 -0500

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name					 "Jeff Jones"
+  user.email                 "Jeff@jones.net"
+  user.password 			 "foobar"
+  user.password_confirmation "foobar"
+end
