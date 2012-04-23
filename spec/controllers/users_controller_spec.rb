a4676edc99d3964043e49896afbebc79165a1e76
Jeff Jones <jjones027@yahoo.com>
2012-04-22 22:18:07 -0400

    Initial interface for following/followers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -115,6 +115,13 @@ describe UsersController do
     
     end
     
+    describe "when signed in as another user" do
+      it "should be successful" do
+        test_sign_in(Factory(:user, :email => Factory.next(:email)))
+        get :show, :id => @user
+        response.should be_success
+      end
+    end
   end
 
   describe "GET 'new'" do
